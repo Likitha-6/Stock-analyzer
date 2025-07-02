fc5e655dc35c93844cd8cf8f47c5f59b26f04ce8
Likitha-6 <likitharouthu@gmail.com>
2025-07-02 01:35:21 -0400

    Update 3_Technical_Analysis.py

--- pages/3_Technical_Analysis.py
+++ pages/3_Technical_Analysis.py
@@ -337,7 +337,7 @@ with tab3:
                 ratings_df = ticker.recommendations
                 #st.write(ratings_df)
 
-                ratings_df["Month"] = ratings_df["Period"].apply(lambda x: x if isinstance(x, str) else str(x))
+                ratings_df["Month"] = ratings_df["period"].apply(lambda x: x if isinstance(x, str) else str(x))
 
                 # Compute Buy, Hold, Sell categories
                 ratings_df["Buy"] = ratings_df["strongBuy"] + ratings_df["buy"]
