2cdd9f0fc16815c63f7d8817947619473516f765
Likitha-6 <likitharouthu@gmail.com>
2025-07-03 14:23:44 -0400

    Update 4_Index_Analysis.py

--- pages/4_Index_Analysis.py
+++ pages/4_Index_Analysis.py
@@ -332,7 +332,7 @@ else:
         score = sentiment["score"]
         color = "green" if label == "positive" else "red" if label == "negative" else "gray"
         st.markdown(
-            f"- **{title}**  \n<span style='color:{color}'>`{label} ({score:.2f})`</span>",
+            f"- **{title}**  \n<span style='color:{color}'>`{label} `</span>",
             unsafe_allow_html=True
         )
 
